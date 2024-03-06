a9ed69237c1460c8cdd8197a7866b4516c1b0ea8
richmondb <richmond.billones@gmail.com>
2024-03-06 21:34:06 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,153 @@
+import Form from 'react-bootstrap/Form';
+import './App.css'
+import Button from 'react-bootstrap/Button';
+import React, {useEffect} from 'react';
+import {TodoItem} from "./components/TodoItem.tsx";
+import Todo from "./interface/Todo.ts";
+
+function App() {
+    const [todos, setTodos] = React.useState<Todo[]>([]);
+    const [todoTitle, setTodoTitle] = React.useState('');
+    const [todoBody, setTodoBody] = React.useState('');
+    const [isEditing, setEditing] = React.useState(false);
+    const [editingIndex, setEditingIndex] = React.useState(0);
+
+    /**
+     * Function to add a new todo item.
+     *
+     * @param {React.FormEvent} e - the form event
+     * @return {void} no return value
+     */
+    const addTodo: (e: React.FormEvent) => void = (e: React.FormEvent): void => {
+        e.preventDefault();
+        console.log(todoTitle, todoBody);
+        // Reset the form input contents
+        setTodos([...todos, {title: todoTitle, body: todoBody, completed: false, date: new Date().toLocaleString()}])
+        setTodoTitle('');
+        setTodoBody('');
+    };
+
+    /**
+     * Removes a todo item at the specified index.
+     *
+     * @param {number} index - The index of the todo item to be removed
+     * @return {void}
+     */
+    const removeTodo: (index: number) => void = (index: number): void => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    /**
+     * Toggle the completion status of a todo at the specified index.
+     *
+     * @param {number} index - The index of the todo to be toggled
+     * @return {void}
+     */
+    const completeTodo: (index: number) => void = (index: number): void => {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    };
+    /**
+     * Edit a specific todo item.
+     *
+     * @param {number} index - The index of the todo item to edit
+     * @return {void}
+     */
+    const editTodo: (index: number) => void = (index: number): void => {
+        const newTodos = [...todos];
+        setTodoTitle(newTodos[index].title);
+        setTodoBody(newTodos[index].body);
+        setEditingIndex(index);
+        setEditing(true);
+    };
+
+    /**
+     * Saves the edit for the specified todo item at the given index.
+     *
+     * @param {number} index - The index of the todo item to be edited
+     * @return {void}
+     */
+    const saveEdit: (index: number) => void = (index: number): void => {
+        const newTodos = [...todos];
+        newTodos[index].title = todoTitle;
+        newTodos[index].body = todoBody;
+        setTodos(newTodos);
+        setTodoTitle('');
+        setTodoBody('');
+        setEditingIndex(0);
+        setEditing(false);
+    };
+
+    /**
+     * Executes the specified effect function when the component mounts, retrieving and setting the 'todos' state from localStorage if it exists.
+     */
+    useEffect(() => {
+        if (localStorage.getItem('todos') !== null) {
+            setTodos(JSON.parse(localStorage.getItem('todos') || '[]'));
+        }
+    }, []);
+
+    /**
+     * Executes the specified effect function when the 'todos' state changes, storing the data in localStorage if 'todos' is not empty.
+     */
+    useEffect(() => {
+        // Store data only if todos is not empty
+        if (todos.length > 0) {
+            localStorage.setItem('todos', JSON.stringify(todos));
+        } else {
+            // Clear localStorage if todos is empty
+            localStorage.removeItem('todos');
+        }
+    }, [todos]);
+
+
+    return (
+        <>
+            <div className='d-relative' style={{width: '600px'}}>
+                <div>
+                    <h1 className='text-center text-lexorange'>Todo List</h1>
+                </div>
+                <div className='p-3 bg-light rounded-3'>
+                    <Form onSubmit={addTodo}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label className={'text-lexpurple'}>Todo Title</Form.Label>
+                            <Form.Control value={todoTitle} onChange={(e) => setTodoTitle(e.target.value)} required
+                                          type="text" placeholder="My Todo Title"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label className={'text-lexpurple'}>Todo Description</Form.Label>
+                            <Form.Control value={todoBody} onChange={(e) => setTodoBody(e.target.value)} as="textarea"
+                                          rows={4} placeholder='My Todo Description'/>
+                        </Form.Group>
+                        <div className='d-flex justify-content-end gap-2'>
+                            {isEditing ?
+                                <Button type='button' variant='info' onClick={() => saveEdit(Number(editingIndex))}>Save
+                                    Edit</Button> : null}
+                            <Button className={'btn-lexorange'} disabled={isEditing} type='submit'>Add Todo</Button>
+                        </div>
+                    </Form>
+                </div>
+
+                <div className='pt-2'>
+                    <h6 className={'text-lexpurple'}>My Todos</h6>
+                </div>
+
+                <div className='d-flex flex-column gap-2 p-3 bg-light rounded-3'>
+
+                    {todos.length === 0 ? <h6 className='text-center fs-3 text-lexorange'>No Todos Yet</h6> : null}
+
+                    {todos.map((todo, index) => (
+                        <TodoItem key={index} index={index} todo={todo} removeTodo={removeTodo} editTodo={editTodo}
+                                  completeTodo={completeTodo} isEditing={isEditing}/>
+                    ))}
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default App
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,59 @@
+import Todo from "../interface/Todo";
+import Button from "react-bootstrap/Button";
+
+export function TodoItem({index, todo, removeTodo, completeTodo, editTodo, isEditing}: {
+    index: number,
+    isEditing: boolean,
+    todo: Todo,
+    removeTodo: (index: number) => void,
+    completeTodo: (index: number) => void,
+    editTodo: (index: number) => void
+}) {
+    return (
+        <div key={index} className='p-2 bg-white border-lexpurple border rounded-2' id='card-container'>
+            <div className='d-flex justify-content-between' id='card-title'>
+                <h6 className={'text-lexpurple'}>{todo.completed ? <del>{todo.title}</del> : todo.title}</h6>
+                <div className='d-flex gap-1'>
+                    <Button variant='success' size='sm' type='button' onClick={() => completeTodo(index)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-check-square" viewBox="0 0 16 16">
+                            <title>Mark as Complete</title>
+                            <path
+                                d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            <path
+                                d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                        </svg>
+                    </Button>
+                    <Button variant='secondary' size='sm' type='button' onClick={() => editTodo(index)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <title>Edit Todo</title>
+                            <path
+                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fillRule="evenodd"
+                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                        </svg>
+                    </Button>
+                    <Button variant='danger' size='sm' type='button' disabled={isEditing} onClick={() => removeTodo(index)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-trash3" viewBox="0 0 16 16">
+                            <title>Delete Todo</title>
+                            <path
+                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                        </svg>
+                    </Button>
+                </div>
+            </div>
+            <div id='card-body'>
+                <p className='w-100 py-1 text-purple text-break text-pretty mb-0'>
+                    {todo.body}
+                </p>
+            </div>
+            <div className={'d-flex justify-content-end'}>
+                <p style={{fontSize: '0.8rem'}} className='mb-0 text-lexorange text-opacity-75'>Date
+                    Created: {todo.date}</p>
+            </div>
+
+        </div>
+    );
+}
